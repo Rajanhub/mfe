4b902cbf6f7ad66e7592ad303580260fff0864fa
Rajan <rajaninspirion@gmail.com>
2022-07-19 17:48:49 +0545

    new commit

--- packages/remote/src/Header.tsx
+++ packages/remote/src/Header.tsx
@@ -1,5 +1,11 @@
+import TagsInput from "./components/TagsInput";
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <TagsInput />
+    </div>
+  );
 }
