1a59f5d028ece4c2dda6bae0a0bab611576639fe
Rajan <rajaninspirion@gmail.com>
2022-07-20 14:03:14 +0545

    asjhdsajh

--- packages/host/config/webpack.prod.js
+++ packages/host/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/host/",
+    publicPath: "/host/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
