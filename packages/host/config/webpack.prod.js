0eb3421ac4e0484ee07e673fcbfa0167bf7a5be2
Rajan <rajaninspirion@gmail.com>
2022-07-20 16:22:42 +0545

    some more changes2

--- packages/host/config/webpack.prod.js
+++ packages/host/config/webpack.prod.js
@@ -14,6 +14,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: "host",
+      filename: "remoteEntry.js",
       remotes: {
         ui: `remote@${domain}/remote/latest/remote.7ababeb1286c4859bd91.js`,
       },
