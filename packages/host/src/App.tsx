fb751629590aced4a25b33d033e45875e6916f22
Rajan <rajaninspirion@gmail.com>
2022-07-20 10:17:29 +0545

    some error fixes

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -14,16 +14,7 @@ const App = () => (
   </div>
 );
 
-const mount = (el: HTMLElement) => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const root = ReactDOM.createRoot(el!);
-  root.render(<App />);
-};
-
-if (process.env.NODE_ENV === "development") {
-  const el = document.getElementById("app") as HTMLElement;
-  if (el) {
-    mount(el);
-  }
-}
-export { mount };
+const el = document.getElementById("app") as HTMLElement;
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = ReactDOM.createRoot(el!);
+root.render(<App />);
